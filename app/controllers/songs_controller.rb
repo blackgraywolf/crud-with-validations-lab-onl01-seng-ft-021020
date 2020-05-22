996c0bde9ac084ffaad042827e7c1840ad1cc16a
industrious-generator-6081 <graywolf.niehaus@live.com>
2020-05-22 16:22:30 +0000

    Automatically backed up by Learn

--- app/controllers/songs_controller.rb
+++ app/controllers/songs_controller.rb
@@ -43,6 +43,7 @@ class SongsController < ApplicationController
   end
 
   private
+  
     def song_params
       params.require(:song).permit(:title, :released, :release_year, :artist_name, :genre)
     end
